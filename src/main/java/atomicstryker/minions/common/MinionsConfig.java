cedd1ff851c7bc5d1fb0e0580ad9f69c7e8c351f
meynoik <neynoi.k@gmail.com>
2026-08-12 18:10:29 +0200

    Add evil deed XP config

--- src/main/java/atomicstryker/minions/common/MinionsConfig.java
+++ src/main/java/atomicstryker/minions/common/MinionsConfig.java
@@ -6,6 +6,7 @@ public final class MinionsConfig {
     public static final ModConfigSpec SPEC;
 
     public static final ModConfigSpec.IntValue MINIONS_PER_PLAYER;
+    public static final ModConfigSpec.IntValue EVIL_DEED_XP_COST;
     public static final ModConfigSpec.DoubleValue FOLLOW_RANGE;
     public static final ModConfigSpec.IntValue MAX_TREE_BLOCKS;
     public static final ModConfigSpec.IntValue MAX_VEIN_BLOCKS;
@@ -21,6 +22,8 @@ public final class MinionsConfig {
         builder.push("general");
         MINIONS_PER_PLAYER = builder.comment("Maximum amount of summoned minions per player.")
                 .defineInRange("minionsPerPlayer", 4, 1, 16);
+        EVIL_DEED_XP_COST = builder.comment("Experience levels paid per evil deed. Set to -1 to disable survival acquisition of the staff.")
+                .defineInRange("evilDeedXPCost", 2, -1, 100);
         FOLLOW_RANGE = builder.comment("Base follow range of minions.")
                 .defineInRange("followRange", 30.0D, 8.0D, 128.0D);
         BIG_EXHAUSTION = builder.comment("Food exhaustion for expensive staff orders.")
